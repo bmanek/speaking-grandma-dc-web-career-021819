f6254780fb30ceeaed8facc9274e100003620fec
Ben Manekin <ben.manekin@gmail.com>
2019-01-23 23:12:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(chatter)
-  if chatter == chatter.
+  if chatter != chatter.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif chatter == "I LOVE YOU, GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
